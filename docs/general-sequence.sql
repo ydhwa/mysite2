5d95ba67d076a38f98a176e08b8c344753b9368a
ydhwa <ydhwa_18@naver.com>
2019-06-11 15:58:29 +0900

    development 환경에서는 postgreSQL 적용

---
+++ docs/general-sequence.sql
@@ -0,0 +1,11 @@
+drop sequence seq_member;
+create sequence seq_member start 1;
+
+drop sequence seq_guestbook;
+create sequence seq_guestbook start 1;
+
+drop sequence seq_board;
+create sequence seq_board start 1;
+
+drop sequence seq_site;
+create sequence seq_site start 1;
