e7708db5e558176980a4bf2fb245f31e3b3b7c38
ydhwa <ydhwa_18@naver.com>
2019-06-25 16:50:02 +0900

    guestbook-ajax, gallery 이사만 시켜둠

--- src/main/java/com/cafe24/mysite/controller/GuestbookController.java
+++ src/main/java/com/cafe24/mysite/controller/GuestbookController.java
@@ -49,5 +49,12 @@ public class GuestbookController {
 		guestbookService.deleteGuestbook(new GuestbookVo(no, password));
 		return "redirect:/guestbook/list";
 	}
+	
+	
+	// 방명록 JSP 페이지
+	@RequestMapping("/ajax")
+	public String indexAjax() {
+		return "guestbook/index-ajax";
+	}
 
 }
